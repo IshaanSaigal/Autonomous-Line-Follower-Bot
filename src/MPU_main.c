0c6b1bfe560053459b5e82086517a9ba8bee1196
IshaanSaigal <saigal.ishaan@gmail.com>
2025-05-19 15:55:08 +0530

    Create MPU_main.c

---
+++ src/MPU_main.c
@@ -0,0 +1,597 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "liquidcrystal_i2c.h"
+
+#include <stdio.h>
+
+#include <math.h>
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define M_PI 3.14159265358979323846
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+I2C_HandleTypeDef hi2c2;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_I2C2_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+int a; //temp
+
+int16_t AccXLSB; //temp
+int16_t AccYLSB;
+int16_t AccZLSB;
+
+float AccX;
+float AccY;
+float AccZ;
+
+float pitch, roll;
+
+
+int16_t GyroX, GyroY, GyroZ;
+float RateX, RateY, RateZ;
+
+//For calibration of gyroscope rate values:
+float RateCalibrationX, RateCalibrationY, RateCalibrationZ;
+int RateCalibrationNumber;
+
+
+//Initial prediction for angle values in zero
+//Uncertainty for the initial guess is 2 degrees.
+float KalmanAngleRoll = 0, KalmanUncertaintyAngleRoll = 2*2;
+float KalmanAnglePitch = 0, KalmanUncertaintyAnglePitch = 2*2;
+
+//1-D Kalman Filter output:
+float Kalman1DOutput[] = {0,0};
+//Kalman1DOutput[0] is angle prediction
+//Kalman1DOutput[1] is uncertainty of the prediction
+
+
+char msg[100];
+
+//MPU6050 Initialization:
+
+void MPU_Init(void) {
+	//Checking if I2C device is ready:
+	HAL_StatusTypeDef ret = HAL_I2C_IsDeviceReady(&hi2c2, (0b1101000<<1)+0, 1, HAL_MAX_DELAY);
+	//0b1101000 is the I2C address according to MPU6050's datasheet.
+	//The above HAL function only accepts bytes, so we add a 0 at the end of the address.
+	//1 => only 1 trial; the microcontroller will try to connect to the I2C device only once.
+	//HAL_StatusTypeDef is is type of the variable ret.
+
+	if (ret == HAL_OK)
+	  a = 1; //temp
+	else
+	  a = 0;
+
+	//HAL_Delay(3000); //temp
+
+	//Values of sensor data is stored on registers in the MPU6050.
+	//We need to read/write to memory to extract this data.
+
+	//We keep the default bandwidths of accelerometer and gyroscope.
+
+	//To set full-scale range of gyroscope as +-500 degree/sec, we need to alter the GYRO_CONFIG register, i.e. register 27.
+	//We must set bit 4 & 3 as 01 respectively.
+
+	uint8_t temp_data = 0b00001000; //value to write to the register
+	ret = HAL_I2C_Mem_Write(&hi2c2, (0b1101000<<1)+0, 27, 1, &temp_data, 1, HAL_MAX_DELAY);
+	//27 is register 27
+	//1 is 1 byte as memory-address size since register 27 is 8-bits.
+	//2nd 1 is size of data we are sending, i.e. 1 byte temp_data.
+
+	if (ret == HAL_OK)
+	  a = 1; //temp
+	else
+	  a = 0;
+
+	//HAL_Delay(3000); //temp
+
+	//To set full-scale range of accelerometer as +-4g, we need to change CONFIG_ACC register, which is register 28.
+	//We must set bit 4 & bit 3 as 01 respectively.
+
+	temp_data = 0b00001000; //value to write to the register
+	ret = HAL_I2C_Mem_Write(&hi2c2, (0b1101000<<1)+0, 28, 1, &temp_data, 1, HAL_MAX_DELAY);
+
+	if (ret == HAL_OK)
+	  a = 1; //temp
+	else
+	  a = 0;
+
+	//HAL_Delay(3000); //temp
+
+	//By default, MPU6050 is in low-power sleep mode.
+	//To exit from this mode, we need to reset bit 6 of register 107 as 0.
+
+	temp_data = 0b00000000; //value to write to the register
+	ret = HAL_I2C_Mem_Write(&hi2c2, (0b1101000<<1)+0, 107, 1, &temp_data, 1, HAL_MAX_DELAY);
+
+	if (ret == HAL_OK)
+	  a = 1; //temp
+	else
+	  a = 0;
+
+	//HAL_Delay(3000); //temp
+
+	//Now initialization is over and we can finally start reading sensor values from registers.
+}
+
+
+void gyroValues(void) {
+	//For finding of gyroscope values
+
+	uint8_t data[2]; //2 byte data buffer to store the register values
+
+	HAL_I2C_Mem_Read(&hi2c2, (0b1101000<<1)+1, 67, 1, data, 2, HAL_MAX_DELAY);
+	GyroX = ((int16_t) data[0] << 8) + data[1];
+
+	HAL_I2C_Mem_Read(&hi2c2, (0b1101000<<1)+1, 69, 1, data, 2, HAL_MAX_DELAY);
+	GyroY = ((int16_t) data[0] << 8) + data[1];
+
+	HAL_I2C_Mem_Read(&hi2c2, (0b1101000<<1)+1, 71, 1, data, 2, HAL_MAX_DELAY);
+	GyroZ = ((int16_t) data[0] << 8) + data[1]; //signed 16-bit value with range: -32,768 to 32,767
+
+	RateX = (float) GyroX / 65.5; //bcs LSB sensitivity is 65.5
+	RateY = (float) GyroY / 65.5;
+	RateZ = (float) GyroZ / 65.5;
+}
+
+
+
+//1-D Kalman Filter:
+
+void Kalman1D(
+		float KalmanState, float KalmanUncertainty, float KalmanInput, float KalmanMeasurement) {
+	//This function calculates the Kalman filter outputs, i.e. the predicted angles and the uncertainties.
+	//KalmanInput: Gyroscope measurements
+	//KalmanMeasurement: accelerometer angle measurement
+	//KalmanState: angle calculated with Kalman filter
+
+	//1. Predict the current state of the system:
+	KalmanState = KalmanState + 0.1 * KalmanInput; //Assuming Ts = 0.004 for measurements of 250Hz.
+
+	//2. Calculate the uncertainty of the prediction:
+	KalmanUncertainty = KalmanUncertainty + 0.1*0.1*4*4;
+
+	//3. Calculated the Kalman Gain from the uncertainties of the predictions and measurements:
+	float KalmanGain = KalmanUncertainty * 1 / (1*KalmanUncertainty + 3*3);
+
+	//4. Update the predicted current state using accelerometer measurements:
+	KalmanState = KalmanState + KalmanGain * (KalmanMeasurement - KalmanState);
+
+	//5. Update the uncertainty on the predicted state;
+	KalmanUncertainty = (1 - KalmanGain) * KalmanUncertainty;
+
+	//6. Kalman filter output:
+	Kalman1DOutput[0] = KalmanState; //prediction of state/angle
+	Kalman1DOutput[1] = KalmanUncertainty; //corresponding uncertainty of the predicted angle
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_I2C1_Init();
+  MX_I2C2_Init();
+  /* USER CODE BEGIN 2 */
+
+  MPU_Init();
+
+
+  //code for lcd:
+  HD44780_Init(2);
+
+  HD44780_Clear();
+  HD44780_SetCursor(0,0);
+  HD44780_PrintStr("PROJECT");
+  HD44780_SetCursor(0,1);
+  HD44780_PrintStr("CRAB");
+  HAL_Delay(1000);
+
+
+  //For calibration of gyroscope values:
+  //We find the sum of 2000 values of the gyroscope
+  //The bot MUST be at rest for this calibration
+  for (RateCalibrationNumber=0; RateCalibrationNumber < 2000; RateCalibrationNumber++) {
+	  gyroValues();
+	  RateCalibrationX += RateX;
+	  RateCalibrationY += RateY;
+	  RateCalibrationZ += RateZ;
+
+	  HAL_Delay(1); //1ms delay for each values => 2s delay overall to find all the sums
+  }
+
+  //Find the average of the 2000 values; these are the average values at rest:
+  RateCalibrationX /= 2000;
+  RateCalibrationY /= 2000;
+  RateCalibrationZ /= 2000;
+
+
+  uint32_t LoopTimer = HAL_GetTick();
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  uint8_t data[2]; //2 byte data buffer to store the register values
+	  HAL_I2C_Mem_Read(&hi2c2, (0b1101000<<1)+1, 59, 1, data, 2, HAL_MAX_DELAY);
+	  //Since we are reading, we set the LSB of I2C address as 1.
+	  //First data register for reading accelerometer values is 59. We only need to pass this first register since we can read following sequential registers.
+	  //1 because every register is 1 byte.
+	  //2 bytes because we want to read registers 59 and 60 for X-axis acceleration.
+
+	  AccXLSB = ((int16_t) data[0] << 8) + data[1];
+
+
+	  HAL_I2C_Mem_Read(&hi2c2, (0b1101000<<1)+1, 61, 1, data, 2, HAL_MAX_DELAY);
+	  AccYLSB = ((int16_t) data[0] << 8) + data[1];
+
+	  HAL_I2C_Mem_Read(&hi2c2, (0b1101000<<1)+1, 63, 1, data, 2, HAL_MAX_DELAY);
+	  AccZLSB = ((int16_t) data[0] << 8) + data[1];
+
+
+	  AccX = (float) AccXLSB / 8129 + 0.15;
+	  AccY = (float) AccYLSB / 8129 - 0.015;
+	  AccZ = (float) AccZLSB / 8129 + 0.07;
+
+
+	  roll = atan(AccY / sqrt(AccZ*AccZ + AccX*AccX) ) * 180/M_PI;
+	  pitch = atan( -AccX / sqrt(AccY*AccY + AccZ*AccZ) ) * 180/M_PI; //atan() returns rad
+
+
+
+	  /*HAL_I2C_Mem_Read(&hi2c2, (0b1101000<<1)+1, 67, 1, data, 2, HAL_MAX_DELAY);
+	  GyroX = ((int16_t) data[0] << 8) + data[1];
+
+	  HAL_I2C_Mem_Read(&hi2c2, (0b1101000<<1)+1, 69, 1, data, 2, HAL_MAX_DELAY);
+	  GyroY = ((int16_t) data[0] << 8) + data[1];
+
+	  HAL_I2C_Mem_Read(&hi2c2, (0b1101000<<1)+1, 71, 1, data, 2, HAL_MAX_DELAY);
+	  GyroZ = ((int16_t) data[0] << 8) + data[1]; //signed 16-bit value with range: -32,768 to 32,767
+
+	  RateX = (float) GyroX / 65.5; //bcs LSB sensitivity is 65.5
+	  RateY = (float) GyroY / 65.5;
+	  RateZ = (float) GyroZ / 65.5;*/
+
+	  gyroValues();
+	  //Get corrected/calibrated rates by subtracting the average values:
+	  RateX -= RateCalibrationX;
+	  RateY -= RateCalibrationY;
+	  RateZ -= RateCalibrationZ;
+
+
+	  //Kalman filtering the values:
+	  Kalman1D(KalmanAngleRoll, KalmanUncertaintyAngleRoll, RateX, roll);
+	  KalmanAngleRoll = Kalman1DOutput[0];
+	  KalmanUncertaintyAngleRoll = Kalman1DOutput[1];
+
+	  Kalman1D(KalmanAnglePitch, KalmanUncertaintyAnglePitch, RateY, pitch);
+	  KalmanAnglePitch = Kalman1DOutput[0];
+	  KalmanUncertaintyAnglePitch = Kalman1DOutput[1];
+
+
+	  //Displaying values on LCD:
+	  HD44780_Clear();
+
+	  HD44780_SetCursor(0,0);
+	  sprintf(msg, "Speed: %.2f", AccY);
+	  HD44780_PrintStr(msg);
+
+	  HD44780_SetCursor(0,1);
+	  sprintf(msg, "Dir: %.2f", pitch);
+	  HD44780_PrintStr(msg);
+
+
+	  while ( (HAL_GetTick() - LoopTimer) < 100);
+	  LoopTimer = HAL_GetTick();
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
